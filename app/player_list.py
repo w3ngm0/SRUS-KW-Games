959ada076d5a61ad5539b964b368547289d68dcc
KeldenWangmo <20070505@tafe.wa.edu.au>
2025-08-12 14:46:49 +0800

    feat(app): Insert method to add new node to list.

--- app/player_list.py
+++ app/player_list.py
@@ -1,7 +1,26 @@
+from player_node import PlayerNode
+
+
 class PlayerList:
-    def __init__(self):
+    def __init__(self, _head: None):
         """Initialize player list."""
-        #Private variable
-        self._head = None
+        self.head = _head
+
+    @property
+    def is_empty(self):
+        return self.head is None
+
+    def insert(self):
+        """Insert a new node at the head of list."""
+        new_node = PlayerNode
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node._next = self.head
+            self.head = new_node
+
+
+
+
 
 
