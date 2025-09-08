54bde8ce68bcbf7ff5fd44b5434230e1a1287479
KeldenWangmo <20070505@tafe.wa.edu.au>
2025-09-09 07:20:41 +0800

    feat(step-5): Add new unittest for Hash Map

---
+++ test/tesh_hash_map.py
@@ -0,0 +1,12 @@
+import unittest
+from player_hashmap import PlayerHashMap
+from player import Player
+
+
+class MyTestCase(unittest.TestCase):
+    def test_player_equality_and_hash(self):
+        """ Test cases go here"""
+        
+
+if __name__ == '__main__':
+    unittest.main()
